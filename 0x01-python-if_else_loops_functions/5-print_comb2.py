ca9c70dde700491671f2c58eaaef69c09bfc7b20
JudahSan <baraka.judah@yahoo.com>
2022-06-01 05:20:29 +0300

    Control structures

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i == 99:
-        print("{:02}".format(i), end="\n")
-        break
-    print("{:02}".format(i), end=", ")
+for num in range(00, 100):
+    print("{:02d}".format(num), end='\n' if num == 99 else ", ")

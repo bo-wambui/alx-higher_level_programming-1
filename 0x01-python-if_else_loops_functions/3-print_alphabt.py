45961d405350242e69fd6167ba9f30d096486c8f
JudahSan <baraka.judah@yahoo.com>
2022-06-01 05:34:05 +0300

    e q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if chr(i) != "e":
-        continue
-    if chr(i) != "q":
-        continue
-    print("{}".format(chr(i)), end="")
+for m in range(97, 123):
+    if (m != 101 and m != 113):
+        print("{}".format(chr(m)), end='')
